233e883593ea3bd3e5b186dd7a4ee84a83ca6ac6
Yulia <ylianna_sko@mail.ru>
2023-04-18 21:07:59 +0300

    three

--- src/main/java/ru/netology/services/HW61/service/Main.java
+++ src/main/java/ru/netology/services/HW61/service/Main.java
@@ -1,4 +1,5 @@
 package ru.netology.services.HW61.service;
+
 public class Main {
     public static void main(String[] args) {
         SqrtService service = new SqrtService();
--- src/test/java/ru/netology/services/HW61/service/SqrtServiceTest.java
+++ src/test/java/ru/netology/services/HW61/service/SqrtServiceTest.java
@@ -1,15 +1,17 @@
 package ru.netology.services.HW61.service;
-import org.junit.jupiter.api.Test;
+
+//import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
+//import org.junit.jupiter.params.provider.CsvSource;
 
 public class SqrtServiceTest {
     @ParameterizedTest
-    @CsvFileSource (files="src/test/resources/rest.csv")
+    @CsvFileSource(files = "src/test/resources/rest.csv")
 
-    public void ToRestMonths( int income, int expenses, int threshold, int expected) {
+    public void ToRestMonths(int income, int expenses, int threshold, int expected) {
         SqrtService service = new SqrtService();
 
         // int income = 10_000;
